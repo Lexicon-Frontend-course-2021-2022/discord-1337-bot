2d36d1a3fa187199d13c81a6fe0993e984b5a1e3
Thomas Hellström <rel@xed.se>
2021-10-25 18:41:20 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,78 @@
+/* ============================================================================
+ * Configuration with reasonable fallbacks for everything except TOKEN
+ * ========================================================================= */
+require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+const CHANNEL = process.env.CHANNEL || 'test';
+const TZ = process.env.TZ || 'UTC';
+const MESSAGE = process.env.MESSAGE || "The time is 13:37";
+const GREETING = process.env.GREETING || "The 1337 bot";
+
+/* ============================================================================
+ * Remember to use `GO_LIVE=true` in .env to enable output to discord. 
+ * If not, all output go to console instead! Great during development :)
+ * ========================================================================= */
+let GO_LIVE = false;
+switch (process.env.GO_LIVE) {
+  case 'true':
+    console.log("GO_LIVE=true : All output go to discord.")
+    GO_LIVE = true;
+    break;
+  default:
+    console.log("GO_LIVE=false : All output go to console.")
+}
+
+/* ============================================================================
+ * Create discord bot
+ * ========================================================================= */
+const { Client } = new require('discord.js');
+const bot = new Client();
+
+/* ============================================================================
+ * Login bot
+ * ========================================================================= */
+bot.login(TOKEN);
+
+/* ============================================================================
+ * When bot is logged in, get channel named CHANNEL and start cronjob to 
+ * send message at 13:37 (In the configured timezone!)
+ * ========================================================================= */
+bot.on('ready', () => {
+
+  // Loop through channel names
+  bot.channels.cache.forEach(item => {
+
+    // This is the correct channel!
+    if (item.name == CHANNEL) {
+
+      // Get channel object
+      bot.channels.fetch(item.id)
+
+        .then(channel => {
+          say(channel, GREETING);
+
+          // Create a new cronjob which prints message in the correct channel
+          const { CronJob } = require('cron');
+
+          const cron = new CronJob(`* 37 13 * * *`, () => {
+            say(MESSAGE);
+
+          }, null, true, TZ);
+
+        });
+    }
+
+  });
+});
+
+/* ============================================================================
+ * Helper function for local development. When BE_QUIET is true, print to 
+ * console instead of spamming discord...
+ * ========================================================================= */
+const say = (channel, msg) => {
+  if (GO_LIVE) {
+    channel.send(msg);
+  } else {
+    console.log(`say: ${msg}`);
+  }
+};
